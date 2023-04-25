7f05956364f5a57d41bec44112de9c6e476177a4
iam-deepakvel <deepakvel82@gmail.com>
2023-04-26 00:29:12 +0530

    fix: env

--- src/constants/games-summary.js
+++ src/constants/games-summary.js
@@ -30,6 +30,7 @@ export const onlineGames = [
     description:
       "Stumble Guys is a thrilling multiplayer game with quirky characters navigating challenging obstacle courses filled with traps and twists. With fast-paced gameplay and unpredictable challenges, it's a test of skill and agility against players from around the world.",
     imageUrl: StumbleGuys,
+    isRegistrationClosed:true
   },
   {
     id: 3,
